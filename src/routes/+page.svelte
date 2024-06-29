8ae2694f83311bd9571be0f62b382ab43a289fc3
Raghad <raghad11ali@gmail.com>
2024-06-29 07:54:56 +0300

    good morning and today1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+<div class="p-12">
+<header class="flex justify-between">
+	<div>
+	<h1 class="text-3xl text-surface-900 mb-2">Good moening🌻</h1>
+	<h2 class="text-surface-900/80">Today Sat 29</h2>
+    </div>
+	<div class="bg-yellow-200 w-12 h-12"></div>
+</header >
 </div>
