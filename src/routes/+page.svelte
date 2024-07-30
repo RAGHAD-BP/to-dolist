74aa7cd02d80512d92d96cda17375c3662710139
Raghad <raghad11ali@gmail.com>
2024-07-30 08:38:58 +0300

    morning and night

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	let period=dayjs().format('A') =='AM'?'morning':'night';
+
+</script>
 <div class="p-12">
-<header class="flex justify-between">
-	<div>
-	<h1 class="text-3xl text-surface-900 mb-2">Good moening🌻</h1>
-	<h2 class="text-surface-900/80">Today Sat 29</h2>
-    </div>
-	<div class="bg-yellow-200 w-12 h-12"></div>
-</header >
+	<header class="flex justify-between">
+		<div>
+			<h1 class="text-3xl text-surface-900 mb-2">Good {period}🌻</h1>
+			<h2 class="text-surface-900/80">{dayjs().format('Today : dddd , D-M-YYYY ')}</h2>
+		<h3 class="text-xl text-surface-900/50">{dayjs().format('h:m A')}</h3>
+		</div>
+		<div class="bg-yellow-200 w-10 h-40"></div>
+	</header >
 </div>
