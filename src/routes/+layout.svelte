944610e01dc5e7bb314a50479b37a6765de048fb
Raghad <raghad11ali@gmail.com>
2024-08-05 10:30:28 +0300

    delete Task❌

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import '../app.postcss';
-
 	// Floating UI for Popups
     import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    import { initializeStores, Modal } from '@skeletonlabs/skeleton';
+    initializeStores();
 </script>
-
+<Modal buttonPositive="bg-yellow-400"/>
 <slot />
